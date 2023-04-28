994e95f3ca1a5e23c4dc904a213228e5a9192900
ij-25 <128509515+ij-25@users.noreply.github.com>
2023-04-28 21:03:42 +0530

    first project

---
+++ script.js
@@ -0,0 +1,17 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': '36f477f7b3mshfb9da360a0319eep17f2e2jsn0f331128773c',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
